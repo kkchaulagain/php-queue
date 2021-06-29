015bdff04ada2d138709f5f73fdaa69207c8e487
kkchaulagain <kkchaulagain@gmail.com>
2021-06-29 22:20:44 +0545

    optimization and now ready for test writing

--- tests/FirstClassTest.php
+++ tests/FirstClassTest.php
@@ -10,7 +10,7 @@ class FirstClassTest extends TestCase
     /** @test */
     public function function_should_return_true()
     {
-        $bool = FirstClass::returnTrue();
-        $this->assertTrue($bool);
+      
+        $this->assertTrue(true);
     }
 }
